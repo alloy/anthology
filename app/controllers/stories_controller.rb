d820dd9a4fc76951c2eff35a5db9647b5f13a13d
Eloy Durán <eloy.de.enige@gmail.com>
2015-07-26 18:04:57 -0400

    [Story] Create a story.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,4 +2,19 @@ class StoriesController < ApplicationController
   def new
     @story = Story.new
   end
+
+  def create
+    @story = Story.new(story_params)
+    if @story.save
+      redirect_to story_url(@story)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def story_params
+    params.require(:story).permit(:writer_id, :cadre_id, :role_id, :feature, :objective)
+  end
 end
--- test/controllers/stories_controller_test.rb
+++ test/controllers/stories_controller_test.rb
@@ -1,9 +1,21 @@
 require_relative '../test_helper'
 
 class StoriesControllerTest < ActionController::TestCase
-  test 'shows a new page' do
+  test 'shows a new story page' do
     get 'new'
     assert_response :ok
     assert_template 'new'
+  end
+
+  test 'creates a story' do
+    assert_difference 'Story.count', +1 do
+      post 'create', story: { writer_id: writers(:eloy).to_param,
+                              cadre_id: cadres(:mobile_engineering).to_param,
+                              role_id: roles(:collector).to_param,
+                              feature: 'to be able to create stories',
+                              objective: 'I can have a better overview of the required work' }
+    end
+    assert_response :found
+    # TODO assert story_url(assigns(:story))
   end
 end
