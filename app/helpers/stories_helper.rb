1a4820cda387d90caa671575258e501f9aa7b2b2
Orta Therox <orta.therox@gmail.com>
2015-07-27 10:57:26 -0400

    add a helper for showing a story

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,2 +1,7 @@
 module StoriesHelper
+  
+  def to_html_sentence(story)
+    "As a <strong>#{story.role.name.downcase}</strong> I want <strong>#{story.feature}</strong>, so that <strong>#{story.objective}.</strong>"
+  end
+  
 end
